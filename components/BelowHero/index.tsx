227fd4679c3491168158ab1e11a76b88d1992892
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2024-07-09 09:23:52 -0700

    aadded

--- components/BelowHero/index.tsx
+++ components/BelowHero/index.tsx
@@ -6,6 +6,7 @@ import { IoCameraSharp } from "react-icons/io5";
 import {IoClipboardOutline} from "react-icons/io5";
 import {FaWpforms} from "react-icons/fa";
 import {IoBookOutline} from "react-icons/io5";
+import Link from "next/link"
 
 
 const AboutSectionLife2 = () => {
@@ -30,20 +31,22 @@ const AboutSectionLife2 = () => {
                                           </button>
                                     </div>
                               </div>
-                        <div className="w-full px-4 lg:w-1/2 xl:w-1/4 transition-transform duration-300 lg:hover:scale-105 cursor-pointer rounded bg-blue-500">
-                        <div className="wow fadeInUp relative mx-auto mb-6 aspect-[25/24] lg:m-0 flex flex-col justify-center items-center text-center" data-wow-delay=".15s">
-                        <FaMoneyCheckDollar className="w-10 h-8 text-black dark:text-white items-center"/>
-                        <h3 className="mb-2 mt-3 text-xl font-bold text-white sm:text-2xl lg:text-xl xl:text-2xl">
-                              Pupils pay code
-                        </h3>
-                        <p className="mb-4 text-base text-white">
-                        Search for your childs mobile pay code for easy transactions.
-                        </p>
-                        
-                        <button className="px-6 py-2 text-white bg-blue-300 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-                         Search
-                        </button>
-                        </div>
+                              <div className="w-full px-4 lg:w-1/2 xl:w-1/4 transition-transform duration-300 lg:hover:scale-105 cursor-pointer rounded bg-blue-500">
+                              <div className="wow fadeInUp relative mx-auto mb-6 aspect-[25/24] lg:m-0 flex flex-col justify-center items-center text-center" data-wow-delay=".15s">
+                              <FaMoneyCheckDollar className="w-10 h-8 text-black dark:text-white items-center"/>
+
+                                    <h3 className="mb-2 mt-3 text-xl font-bold text-white sm:text-2xl lg:text-xl xl:text-2xl">
+                                          Pupils pay code
+                                    </h3>
+                              <p className="mb-4 text-base text-white">
+                              Search for your childs mobile pay code for easy transactions.
+                              </p>
+                              <Link href={"/codepay"} >
+                                    <button className="px-6 py-2 text-white bg-blue-300 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+                                    Search
+                                    </button>
+                              </Link>
+                              </div>
                         </div>
                         <div className="w-full px-4 lg:w-1/2 xl:w-1/4 transition-transform duration-300 lg:hover:scale-105 cursor-pointer rounded bg-blue-300">
                         <div className="wow fadeInUp relative mx-auto mb-6 aspect-[25/24] lg:m-0 flex flex-col justify-center items-center text-center" data-wow-delay=".15s">
