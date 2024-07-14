7a7738dfa440061b5556b0f0d1e27d96ac980570
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2024-07-13 23:10:09 -0700

    more today

--- components/About/AboutSectionAccommodation.tsx
+++ components/About/AboutSectionAccommodation.tsx
@@ -13,10 +13,10 @@ const AboutSectionAcademic = () => {
     <section className="py-16 md:py-20 lg:py-28">
 
     <div className="container -mb-20">
-        <h3 className=" -mt-20 text-xl font-bold text-black dark:text-white sm:text-3xl lg:text-xl xl:text-4xl text-center">
+        <h3 className="mt-4 -mt-20 text-xl font-bold text-black dark:text-white sm:text-3xl lg:text-xl xl:text-4xl text-center">
            Excellence in research, teaching, and medical care
         </h3>
-        <div className="flex items-center gap-10 -mb-8">
+        <div className="flex flex-col lg:flex-row items-center gap-10 -mb-8">
           <div
             className="wow h-[500px] fadeInUp relative mx-auto text-center lg:m-0 transition-transform duration-300 hover:scale-105 cursor-pointer"
             data-wow-delay=".15s" 
@@ -24,10 +24,10 @@ const AboutSectionAcademic = () => {
             <Image
               src={image}
               alt="Shining Stars Building"
-              className="w-[500px] h-[300px]"
+              className="w-[500px] h-[300px] lg:mt-12 -mb-10"
             />
           </div>
-          <div className="w-full px-2 lg:w-1/2 h-[500px] mt-4">
+          <div className="w-full px-2 lg:w-1/2 h-[500px] mt-4 bg-green-200">
             <h3 className="mb-5 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
             Academics
             </h3>
@@ -40,8 +40,8 @@ const AboutSectionAcademic = () => {
           </div>
         </div>
 
-        <div className="flex items-center gap-10 -mt-20">
-          <div className="w-full px-2 lg:w-1/2 h-[500px] mt-5">
+        <div className="flex flex-col lg:flex-row items-center gap-10 -mt-20 ">
+          <div className="w-full px-2 lg:w-1/2 h-[500px] mt-5 bg-green-200">
             <h3 className="mb-5 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
             ........
             </h3>
