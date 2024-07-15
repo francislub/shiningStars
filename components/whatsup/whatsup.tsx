fa39d27d09b925afe4442c73d1616639c920d107
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2024-07-15 10:25:21 -0700

    whatsup

---
+++ components/whatsup/whatsup.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { FaWhatsapp } from "react-icons/fa";
+
+export default function WhatsApp() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 100) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  const openWhatsApp = () => {
+    window.open("https://wa.me/+256762939942", "_blank");
+  };
+
+  return (
+    <div className="fixed bottom-8 left-8 z-50">
+      {isVisible && (
+        <div
+          className="whatsapp-button bg-green-500 hover:bg-green-600 rounded-full p-3 cursor-pointer shadow-lg"
+          onClick={openWhatsApp}
+        >
+          <FaWhatsapp className="text-white" size={32} />
+        </div>
+      )}
+    </div>
+  );
+}
