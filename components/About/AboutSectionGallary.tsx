46fa97b19dc859612eff6287b3c630bfbac39f32
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2024-07-19 09:15:07 -0700

    gallary

--- components/About/AboutSectionGallary.tsx
+++ components/About/AboutSectionGallary.tsx
@@ -2,42 +2,124 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import { BookOpenIcon, GlobeAltIcon, StarIcon, EyeIcon } from '@heroicons/react/solid';
 
-const fetchImageUrls = async (albumLink) => {
-  // Mock API call - Replace this with your actual API call logic
-  return [
-    "https://lh3.googleusercontent.com/your-image-url1.jpg",
-    "https://lh3.googleusercontent.com/your-image-url2.jpg",
-    "https://lh3.googleusercontent.com/your-image-url3.jpg",
-    // Add more URLs as needed
-  ];
-};
+import basketa from "../../public/images/shi/life.jpeg";
+import basketb from "../../public/images/shi/mddfz.jpeg";
+import basketc from "../../public/images/shi/pip.jpeg";
+import basketd from "../../public/images/shi/mdgd.jpeg";
+import grad from "../../public/images/shi/schoo.jpeg";
 
 const AboutSectionGallery = () => {
-  const [imageUrls, setImageUrls] = useState([]);
-
-  useEffect(() => {
-    const getImages = async () => {
-      const urls = await fetchImageUrls("https://photos.google.com/share/AF1QipM3aWmWdnZT28nqVVjQPo3KNfKz0NIjkF7Od8Ap73blWXzz-Pz1CHERvhnHC8zjcg?key=QWxpUkNxNW5BTHZCMDE3czEzMXpIWkFLWE9LUFpB");
-      setImageUrls(urls);
-    };
-
-    getImages();
-  }, []);
-
+ 
   return (
     <section className="py-16 md:py-20 lg:py-28">
       <div className="container -mb-20">
         <h3 className="mt-4 lg:-mt-20 text-xl font-bold text-black dark:text-white sm:text-3xl lg:text-xl xl:text-4xl text-center">
-          Excellence in research, teaching, and medical care
+        Welcome to Shining Stars Primary School Gallery!
         </h3>
 
-        <div className="flex flex-wrap items-center gap-10 -mt-20">
-          {imageUrls.map((url, index) => (
-            <div key={index} className="w-full sm:w-1/2 lg:w-1/3">
-              <Image src={url} alt={`Gallery Image ${index + 1}`} width={500} height={500} className="rounded-lg object-cover" />
+        <h3 className="mb-2 text-xl font-bold text-black text-center sm:text-2xl lg:text-xl xl:text-2xl">
+        Discover Our School Life Through Our Gallery
+          </h3>
+
+          <div className="flex flex-wrap -mx-4 mb-7">
+           <div className="w-full px-4 lg:w-2/5 transition-transform duration-300 hover:scale-105 cursor-pointer">
+            <div className="wow fadeInUp relative mx-auto lg:m-0" data-wow-delay=".15s">
+              <Image
+                src={grad}
+                alt="about image 1"
+                className="w-[250px] h-[130px] sm:w-[250px] sm:h-[100px] md:w-[300px] md:h-[150px] lg:w-[300px] lg:h-[180px] xl:w-[350px] xl:h-[200px]"
+              />
             </div>
-          ))}
-        </div>
+          </div>
+            
+            <div className="w-full px-4 lg:w-3/5 flex flex-col bg-green-200">
+              <div className="wow fadeInUp relative mx-auto lg:m-0" data-wow-delay=".15s">
+                <p className="mb-4 text-base text-gray-600">
+                Explore the dynamic world of Shining Stars Primary School through our vibrant gallery. Here, you&apos;ll find captivating images from our engaging educational trips, inspiring tours, and the lively school environment that makes our community unique. Each photo tells a story of our pupil&apos;s adventures, learning experiences, and the nurturing atmosphere we proudly offer. Dive into the moments that highlight the essence of our school&apos;s commitment to enriching young minds.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-wrap -mx-4">
+            <div className="w-full px-4 lg:w-1/4 transition-transform duration-300 hover:scale-105 cursor-pointer">
+              <div className="wow fadeInUp relative mx-auto mb-12 aspect-[25/24] text-center lg:m-0" data-wow-delay=".15s">
+                <Image
+                  src={basketa}
+                  alt="about image 1"
+                  className="w-full h-auto"
+                />
+                {/* <h3 className="mb-5 mt-3 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
+                  A matter of debate.
+                </h3>
+                <p className="mb-4 text-base text-gray-600 dark:text-gray-300">
+                  Join the debating society at Bugema University.
+                </p> */}
+                {/* <button className="px-6 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+                  Bugema Debating Union
+                </button> */}
+              </div>
+            </div>
+
+            <div className="w-full px-4 lg:w-1/4 transition-transform duration-300 hover:scale-105 cursor-pointer">
+              <div className="wow fadeInUp relative mx-auto mb-12 aspect-[25/24] text-center lg:m-0" data-wow-delay=".15s">
+                <Image
+                  src={basketb}
+                  alt="about image 2"
+                  className="w-full h-auto"
+                />
+                {/* <h3 className="mb-5 mt-3 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
+                  A matter of debate.
+                </h3>
+                <p className="mb-4 text-base text-gray-600 dark:text-gray-300">
+                  Join the debating society at Bugema University.
+                </p> */}
+                {/* <button className="px-6 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+                  Bugema Debating Union
+                </button> */}
+              </div>
+            </div>
+
+            <div className="w-full px-4 lg:w-1/4 transition-transform duration-300 hover:scale-105 cursor-pointer">
+              <div className="wow fadeInUp relative mx-auto mb-12 aspect-[25/24] text-center lg:m-0" data-wow-delay=".15s">
+                <Image
+                  src={basketc}
+                  alt="about image 3"
+                  className="w-full h-auto"
+                />
+                {/* <h3 className="mb-5 mt-3 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
+                  A matter of debate.
+                </h3>
+                <p className="mb-4 text-base text-gray-600 dark:text-gray-300">
+                  Join the debating society at Bugema University.
+                </p> */}
+                {/* <button className="px-6 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+                  Bugema Debating Union
+                </button> */}
+              </div>
+            </div>
+
+            <div className="w-full px-4 lg:w-1/4 transition-transform duration-300 hover:scale-105 cursor-pointer">
+              <div className="wow fadeInUp relative mx-auto mb-12 aspect-[25/24] text-center lg:m-0" data-wow-delay=".15s">
+                <Image
+                  src={basketd}
+                  alt="about image 4"
+                  className="w-full h-auto"
+                />
+                {/* <h3 className="mb-5 mt-3 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
+                  A matter of debate.
+                </h3>
+                <p className="mb-4 text-base text-gray-600 dark:text-gray-300">
+                  Join the debating society at Bugema University.
+                </p> */}
+                {/* <button className="px-6 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+                  Bugema Debating Union
+                </button> */}
+              </div>
+            </div>
+          </div>
+          {/* ########END######### */}
+          
       </div>
     </section>
   );
