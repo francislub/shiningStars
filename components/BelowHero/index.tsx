69e12bd3abe5ebccf0c76410595dbfd763c7b124
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2024-08-09 04:57:43 -0700

    more

--- components/BelowHero/index.tsx
+++ components/BelowHero/index.tsx
@@ -6,16 +6,34 @@ import { IoCameraSharp } from "react-icons/io5";
 import {IoClipboardOutline} from "react-icons/io5";
 import {FaWpforms} from "react-icons/fa";
 import {IoBookOutline} from "react-icons/io5";
+import { useState,useEffect } from "react";
 import Link from "next/link"
 
 
 const AboutSectionLife2 = () => {
+      const [animate, setAnimate] = useState(false);
+
+      const animated = () => {
+            if (window.scrollY >= 1) {
+                  setAnimate(true);
+            } else {
+                  setAnimate(false);
+            }
+          };
+          useEffect(() => {
+            window.addEventListener("scroll", animated);
+            return () => {
+              window.removeEventListener("scroll", animated);
+            };
+          }, []);
+
   return (
     <section className="top-0">
             <div className="container  -my-24">
                   <div className="container mx-auto">
                         <div className="flex flex-wrap -mx-4 gap-2 lg:gap-0">
-                              <div className="w-full px-4 lg:w-1/2 xl:w-1/4 transition-transform duration-300 lg:hover:scale-105 cursor-pointer rounded bg-yellow-700">
+                        <div className={`w-full px-4 lg:w-1/2 xl:w-1/4 transition-transform duration-300 lg:hover:scale-105 cursor-pointer rounded bg-yellow-700 ${animate ?"slider-up slide-up-faster":"" }`}>
+                              {/* <div className={'w-full px-4 lg:w-1/2 xl:w-1/4 transition-transform duration-300 lg:hover:scale-105 cursor-pointer rounded bg-yellow-700 ${animate ?"slider-up slide-up-faster":"" }`} > */}
                                     <div className="wow fadeInUp relative mx-auto mb-6 aspect-[25/24] lg:m-0 flex flex-col justify-center items-center text-center" data-wow-delay=".15s ">
 
                                           <FaWpforms className="w-10 h-8  text-black dark:text-white items-center"/>
@@ -33,7 +51,9 @@ const AboutSectionLife2 = () => {
                                           </Link>
                                     </div>
                               </div>
-                              <div className="w-full px-4 lg:w-1/2 xl:w-1/4 transition-transform duration-300 lg:hover:scale-105 cursor-pointer rounded bg-yellow-600">
+                              <div className={`w-full px-4 lg:w-1/2 xl:w-1/4 transition-transform duration-300 lg:hover:scale-105 cursor-pointer rounded bg-yellow-600 ${animate ?"slider-up slide-up-faster":"" }`}>
+
+                              {/* <div className="w-full px-4 lg:w-1/2 xl:w-1/4 transition-transform duration-300 lg:hover:scale-105 cursor-pointer rounded bg-yellow-600 "> */}
                               <div className="wow fadeInUp relative mx-auto mb-6 aspect-[25/24] lg:m-0 flex flex-col justify-center items-center text-center" data-wow-delay=".15s">
                               <FaMoneyCheckDollar className="w-10 h-8 text-black dark:text-white items-center"/>
 
@@ -50,7 +70,10 @@ const AboutSectionLife2 = () => {
                               </Link>
                               </div>
                         </div>
-                        <div className="w-full px-4 lg:w-1/2 xl:w-1/4 transition-transform duration-300 lg:hover:scale-105 cursor-pointer rounded bg-yellow-700">
+                        <div className={`w-full px-4 lg:w-1/2 xl:w-1/4 transition-transform duration-300 lg:hover:scale-105 cursor-pointer rounded bg-yellow-700 ${animate ?"slider-up slide-up-faster":"" }`}>
+
+                        {/* <div className="w-full px-4 lg:w-1/2 xl:w-1/4 transition-transform duration-300 lg:hover:scale-105 cursor-pointer rounded bg-yellow-700"> */}
+                        
                         <div className="wow fadeInUp relative mx-auto mb-6 aspect-[25/24] lg:m-0 flex flex-col justify-center items-center text-center" data-wow-delay=".15s">
                         <IoBookOutline className="w-10 h-8 text-black dark:text-white items-center"/>
                         <h3 className="mb-2 mt-3 text-xl font-bold text-white sm:text-2xl lg:text-xl xl:text-2xl">
@@ -66,7 +89,10 @@ const AboutSectionLife2 = () => {
                         </Link>
                         </div>
                         </div>
-                        <div className="w-full px-4 lg:w-1/2 xl:w-1/4 transition-transform duration-300 lg:hover:scale-105 cursor-pointer rounded bg-yellow-600 ">
+                        <div className={`w-full px-4 lg:w-1/2 xl:w-1/4 transition-transform duration-300 lg:hover:scale-105 cursor-pointer rounded bg-yellow-600 ${animate ?"slider-up slide-up-faster":"" }`}>
+                        {/* </div>
+                        <div className="w-full px-4 lg:w-1/2 xl:w-1/4 transition-transform duration-300 lg:hover:scale-105 cursor-pointer rounded bg-yellow-600 "> */}
+
                         <div className="wow fadeInUp relative mx-auto mb-12 aspect-[25/24] lg:m-0 flex flex-col justify-center items-center text-center" data-wow-delay=".15s">
                         <FaScroll className="w-10 h-8 text-black dark:text-white "/>
                         <h3 className="mb-2 mt-3 text-xl font-bold text-white sm:text-2xl lg:text-xl xl:text-2xl">
