bfcfa65e44df2e2a0c431cd3799a2a6b1176570a
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2024-08-20 23:26:27 -0700

    ADDED

--- components/calender/calender.tsx
+++ components/calender/calender.tsx
@@ -22,13 +22,13 @@ export default function Calender() {
   }, []);
 
   return (
-    <div className="fixed bottom-8 left-8 z-[99]">
+    <div className="fixed bottom-8 left-1/2 transform -translate-x-1/2 z-[99]">
       {isVisible && (
         <Link
           href={'/calendar'}
           // onClick={() => setIsPopupVisible(!isPopupVisible)}
           aria-label="scroll to top"
-          className="flex px-1 h-10 w-[240px] cursor-pointer items-center justify-center rounded-md bg-primary text-white shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
+          className="flex px-1 h-10 w-[140px] cursor-pointer items-center justify-center rounded-md bg-primary text-white shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
         >
           <span className="text-white">Our Calendar</span>
 
