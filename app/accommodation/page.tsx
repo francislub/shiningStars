58c096fdbbad516da4a457963963e665eaa35742
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2025-08-27 00:32:35 -0700

    Fast more

--- app/accommodation/page.tsx
+++ app/accommodation/page.tsx
@@ -15,7 +15,7 @@ const AboutPage = () => {
       <Breadcrumb
         pageName="Accommodation"
         description="
-        Shining Stars is a place for everyone, somewhere you can be yourself. Whether you're an out-of-the-box thinker, boundary-breaker or change-maker, this is where you'll get ahead and find your place as part of a global community.
+        Shining Stars vvumba is a place for everyone, somewhere you can be yourself. Whether you're an out-of-the-box thinker, boundary-breaker or change-maker, this is where you'll get ahead and find your place as part of a global community.
         "
       />
       {/* <AboutSectionSix /> */}
